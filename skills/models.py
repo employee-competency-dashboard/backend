d32f1b52da446300b3d29cd954e29e34f662d39d
Serhii Vinogradov <123404466+yan-gabala@users.noreply.github.com>
2024-10-08 18:33:18 +0300

    feature: Add part model Skill

--- skills/models.py
+++ skills/models.py
@@ -2,10 +2,31 @@ from django.conf import settings
 from django.db import models
 
 
+class Skill(models.Model):
+    'Модель навыка сотрудника'
+
+    skill_name = models.CharField(
+        choices=settings.SKILL,
+        verbose_name='Название навыка',
+    )
+    skill_type = models.CharField(
+        choices=settings.TYPE,
+        verbose_name='Тип навыка',
+    )
+    id_expertise  = models.ManyToManyField(
+
+    )
+
+
+
+
+
+
+
 class Grade(models.Model):
     'Модель грейда сотрудника'
 
-    grade = models.TextField(
+    grade = models.CharField(
         choices=settings.GRADE,
         verbose_name='Грэйд',
     )
